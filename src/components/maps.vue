c2c618c997ddd6973c8b9bedd2a671ec23e0563f
geekape <zhongchubing@122-122-122-103.dynamic-ip.hinet.net>
2018-12-20 08:12:00 +0800

    :star: 跑步页

---
+++ src/components/maps.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  map#map.maps(:longitude="longitude" :latitude="latitude" :markers="markers" show-compass :circles="circles")
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      latitude: null,
+      longitude: null,
+      circles: [],
+      markers:[]
+    }
+  },
+  created () {
+    const _this = this
+    wx.getLocation({
+      type: 'wgs84',
+        success: function(res){
+          let markers = {
+            latitude: res.latitude,
+            longitude: res.longitude,
+            iconPath: '/static/images/icon-marker.png',
+            width: 30,
+            height: 30
+          }
+          _this.latitude = res.latitude
+          _this.longitude = res.longitude
+          _this.markers.push(markers)
+        }  
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .maps {
+    width: 100%;
+    min-height: 60vh;
+  }
+</style>
---
+++ src/pages/run.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+    .run
+      maps
+      .run-info
+        .run-info__km 0.00
+          span.km.f-font-xs 公里
+        .run-info__row
+          .run-info__item
+            .run-info__title.f-font-xs  运动时间
+            .run-info__number 00:45
+          .run-info__item
+            .run-info__title.f-font-xs  配速
+            .run-info__number 5'8
+          .run-info__item
+            .run-info__title.f-font-xs  卡路里
+            .run-info__number 180
+              span.f-font-xs 千卡
+          
+      
+</template>
+
+<script>
+import maps from '@/components/maps'
+export default {
+  mpType: 'page',
+  data () {
+    return {
+      
+    }
+  },
+
+  components: {
+    maps
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    
+  },
+
+  created () {
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/style.scss';
+.run {
+  background: #fff;
+}
+.run-info {
+  padding: 15px;
+  height: 40vh;
+  &__km {
+    font-size: 48px;
+    text-align: center;
+    .km {margin-left: 5px;}
+  }
+
+  &__row {
+    display: flex;
+    margin-top: 20px;
+  }
+  &__item {
+    flex: 1;
+  }
+
+  &__title {
+    color: $gray;
+  }
+
+  &__number {
+    font-size: 24px;
+  }
+
+}
+
+</style>
